a9695fdf67eecb8d57fc30a366861a48093c35fc
k-web-tech <kamrulislamapurba@gmail.com>
2025-07-13 00:08:05 +0600

    Admition, review and more api added

--- index.js
+++ index.js
@@ -41,52 +41,84 @@ async function run() {
     try {
 
         // ----------------collection------------------
-        const PostsCollection = client.db("kblogify").collection('posts')
+        const CollegeCollection = client.db("K-EduNest").collection('Colleges')
+        const AdmissionCollection = client.db("K-EduNest").collection('Admission')
+        const ReviewsCollection = client.db("K-EduNest").collection('Reviews')
 
 
         // ----------------------------------------------------------------------------------------
-        //------all POSTS ---------
+        //------all College ---------
         // ----------------------------------------------------------------------------------------
 
-        // ---------------get all POSTS----------------
-        app.get("/Posts", async (req, res) => {
-            const result = await PostsCollection.find().toArray();
+        // ---------------get all College----------------
+        app.get("/College", async (req, res) => {
+            const result = await CollegeCollection.find().toArray();
             res.send(result)
         })
 
-        // ----------------------get POSTS by id -----------------------------
-        app.get("/POSTS/:id", async (req, res) => {
+        // ----------------------get College by id -----------------------------
+        app.get("/College/:id", async (req, res) => {
             const id = req.params.id
             const query = { _id: new ObjectId(id) }
 
-            const result = await PostsCollection.findOne(query)
+            const result = await CollegeCollection.findOne(query)
             res.send(result)
         })
 
 
 
-        //----------add POSTS--------
-        app.post('/Posts', async (req, res) => {
+        //----------add College--------
+        app.post('/College', async (req, res) => {
             const review = req.body
-            const result = await PostsCollection.insertOne(review)
+            const result = await CollegeCollection.insertOne(review)
             res.send(result)
         })
 
-        //---------- DELETE POSTSs by id----------------
-        app.delete("/POSTS/:id", async (req, res) => {
+        //---------- DELETE Colleges by id----------------
+        app.delete("/College/:id", async (req, res) => {
             const { id } = req.params;
             try {
-                const result = await PostsCollection.deleteOne({ _id: new ObjectId(id) });
+                const result = await CollegeCollection.deleteOne({ _id: new ObjectId(id) });
                 if (result.deletedCount === 1) {
-                    res.status(200).json({ message: "POSTS deleted successfully." });
+                    res.status(200).json({ message: "College deleted successfully." });
                 } else {
-                    res.status(404).json({ message: "POSTS not found." });
+                    res.status(404).json({ message: "College not found." });
                 }
             } catch (err) {
                 console.error("Delete Error:", err);
                 res.status(500).json({ message: "Server error." });
             }
         });
+
+
+        // ---------------------------------------------------------
+        // --admission----
+        // --------------------------------------------------------
+
+
+        app.post('/admissions', async (req, res) => {
+            const admission = req.body;
+            if (!admission.collegeId) return res.status(400).json({ error: 'College ID is required' });
+
+            try {
+                const result = await AdmissionCollection.insertOne({
+                    ...admission,
+                    collegeId: new ObjectId(admission.collegeId),
+                    createdAt: new Date(),
+                });
+                res.status(201).json({ message: 'Admission submitted', id: result.insertedId });
+            } catch (error) {
+                res.status(500).json({ error: 'Failed to submit admission' });
+            }
+        });
+
+
+        app.get('/admissions', async (req, res) => {
+            const result = await AdmissionCollection.find().toArray();
+            res.send(result)
+        });
+
+
 
 
 
@@ -185,38 +217,38 @@ async function run() {
         // });
 
 
-        //--------------------------------------------------------------------
-        //-------------Reviews----------------
-        //---------------------------------------------------------------------
+        // --------------------------------------------------------------------
+        // ------------- Reviews----------------
+        // ---------------------------------------------------------------------
 
-        // ---------------get all Review----------------
-        // app.get("/reviews", async (req, res) => {
-        //     const result = await ReviewsCollection.find().toArray();
-        //     res.send(result)
-        // })
+        // --------------- get all Review----------------
+        app.get("/review", async (req, res) => {
+            const result = await ReviewsCollection.find().toArray();
+            res.send(result)
+        })
 
-        //----------add review--------
-        // app.post('/reviews', async (req, res) => {
-        //     const review = req.body
-        //     const result = await ReviewsCollection.insertOne(review)
-        //     res.send(result)
-        // })
+        // ----------add review--------
+        app.post('/review', async (req, res) => {
+            const review = req.body
+            const result = await ReviewsCollection.insertOne(review)
+            res.send(result)
+        })
 
-        //---------- DELETE review by id----------------
-        // app.delete("/Reviews/:id", async (req, res) => {
-        //     const { id } = req.params;
-        //     try {
-        //         const result = await ReviewsCollection.deleteOne({ _id: new ObjectId(id) });
-        //         if (result.deletedCount === 1) {
-        //             res.status(200).json({ message: "Review deleted successfully." });
-        //         } else {
-        //             res.status(404).json({ message: "Review not found." });
-        //         }
-        //     } catch (err) {
-        //         console.error("Delete Error:", err);
-        //         res.status(500).json({ message: "Server error." });
-        //     }
-        // });
+        // ----------DELETE review by id----------------
+        app.delete("/review/:id", async (req, res) => {
+            const { id } = req.params;
+            try {
+                const result = await ReviewsCollection.deleteOne({ _id: new ObjectId(id) });
+                if (result.deletedCount === 1) {
+                    res.status(200).json({ message: "Review deleted successfully." });
+                } else {
+                    res.status(404).json({ message: "Review not found." });
+                }
+            } catch (err) {
+                console.error("Delete Error:", err);
+                res.status(500).json({ message: "Server error." });
+            }
+        });
 
     }
     finally {
@@ -227,7 +259,7 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('The blogs are here')
+    res.send('The Colleges are here')
 })
 
 app.listen(port)
